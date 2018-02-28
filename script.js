0f17b7007881102c1d472711a733052ea0ceb675
Marc <marc.casellas@est.fib.upc.edu>
2018-02-28 15:32:42 +0100

    C2

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ var queryInput = document.querySelector('#query'),
 
 
 function update_play_pausa() {
-    
-    
+
+
         playButton.innerHTML = audioTag.paused ? 'Play' : 'Pause';
         playButton.classList = audioTag.paused ? 'boton button special icon fa-play' : 'boton button special icon fa-pause';
-    
-    
-    
-   
+
+
+
+
 }
 
 audioTag.addEventListener('play', update_play_pausa);
@@ -41,54 +41,56 @@ playButton.addEventListener('click', function() {
 
 result.style.display = 'none';
 
-document.querySelector('form').addEventListener('submit', function(formEvent) {
+document.querySelector('form').addEventListener('submit', hello);
+
+function hello (formEvent) {
   formEvent.preventDefault();
   result.style.display = 'none';
-    
-    
-  spotifyApi.searchTracks(queryInput.value.trim(), {limit: 1})
+  var song = 'Thunderstruck'
+
+  spotifyApi.searchTracks(song.trim(), {limit: 1})
     .then(function(results) {
       var track = results.tracks.items[0];
-      
-      
+
+
       // Obtenim les Audio Features
     spotifyApi.getAudioFeaturesForTrack(track.id)
         .then(function(results) {
             var afeature = results.audio_features[0];
-   
-        
 
-      
 
-    
+
+
+
+
       // Selecció de la preview de la música
       if (track.preview_url != null) var previewUrl = track.preview_url;
       else var previewUrl = "./assets/audio.mp3";
       audioTag.src = previewUrl;
-      
-      
-      
+
+
+
       // Iniciem la request
       var request = new XMLHttpRequest();
       request.open('GET', previewUrl, true);
       request.responseType = 'arraybuffer';
       request.onload = function() {
-         
+
 
           text.innerHTML = '<h2>' + track.name + '<br></h2>' +
             '<h3>' + track.artists[0].name + '</h3>';
 
           caratula.src = track.album.images[1].url;
-          
+
         info.innerHTML = 'Energy: ' + afeature.energy*100 + '% <br>'
     + 'Danceability: ' + afeature.danceability*100 + '%';
-           
+
           result.style.display = 'block';
-          
-          
-      
+
+
+
       };
       request.send();
     });
           });
-});
+};
